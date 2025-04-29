db4877a0dfaba0d5285678abbbf81557deee6ee6
Qrun338 <wiktorkuranowski@gmail.com>
2025-04-29 17:38:14 +0200

    Front do alertow i klasa

--- models.py
+++ models.py
@@ -18,7 +18,13 @@ class FavouriteFlight(db.Model):
     departure_airport = db.Column(db.String(50))
     arrival_airport = db.Column(db.String(50))
 
-
+class FlightAlert(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    flight_number = db.Column(db.String(50), nullable=False)
+    condition_type = db.Column(db.String(50)) 
+    threshold = db.Column(db.String(50))       
+    triggered = db.Column(db.Boolean, default=False)
 
     
 def __repr__(self):
